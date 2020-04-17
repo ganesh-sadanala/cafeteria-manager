d0381ef496f871e6f98b115d618b5129aa907e75
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-04-17 09:12:28 +0530

    create method creates a new menu

--- Cafeteria_manager/app/controllers/menus_controller.rb
+++ Cafeteria_manager/app/controllers/menus_controller.rb
@@ -8,4 +8,13 @@ class MenusController < ApplicationController
     menu_items = MenuItem.where("menu_id=?", id)
     render plain: menu_items.all.map { |menu_item| menu_item.to_pleasant_string }.join("\n")
   end
+
+  def create
+    id = params[:menu_id]
+    name = params[:menu_name]
+    new_menu = Menu.create!(
+      menu_id: id,
+      menu_name: name,
+    )
+  end
 end
