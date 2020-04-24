2e212fff06f9402b59e9d027e7e28a492b36ffdc
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-04-24 11:41:27 +0530

    User can add a menu_item and store it in the database

--- Cafeteria_manager/app/controllers/menu_Items_controller.rb
+++ Cafeteria_manager/app/controllers/menu_Items_controller.rb
@@ -1,2 +1,9 @@
 class MenuItemsController < ApplicationController
+  def create
+    id = params[:menu_id]
+    item_name = params[:menu_item_name]
+    item_price = params[:menu_item_price]
+    MenuItem.create!(menu_id: id, menu_item_name: item_name, menu_item_price: item_price)
+    redirect_to menus_path
+  end
 end
