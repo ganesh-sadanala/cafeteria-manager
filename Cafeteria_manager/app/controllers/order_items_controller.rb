be2d26d1e29fed9072964f632869d5692f8bc495
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-05-05 16:50:59 +0530

    User can place an order with some menu-items

--- Cafeteria_manager/app/controllers/order_items_controller.rb
+++ Cafeteria_manager/app/controllers/order_items_controller.rb
@@ -1,12 +1,4 @@
 class OrderItemsController < ApplicationController
   def create
-    item_ids = params[:item_ids]
-    clean_item_ids = item_ids - [nil]
-    Order.create!(
-      user_id: current_user.id,
-      date: Date.Today,
-    )
-    clean_item_ids.each { |item| }
-    redirect_to orders_path
   end
 end
