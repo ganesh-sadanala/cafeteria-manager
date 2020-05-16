d0391f2b13d9892616acf077fd334c5fcc7630c5
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-05-16 21:10:13 +0530

    Bug analysis

--- Cafeteria_manager/config/routes.rb
+++ Cafeteria_manager/config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/" => "home#index"
   resources :menu_items
   resources :menus
   resources :users
   resources :orders
   resources :admin
-  get "/active" => "menus#active_menus", as: :active_menus
-  get "/cart" => "orders#show_cart", as: :cart_orders
   resources :order_items
+  get "/" => "home#index"
+  get "/active" => "menus#active_menus", as: :active_menus
+  get "/cart" => "orders#show_cart", as: :show_cart
+  post "/cart-order" => "orders#cart_to_order", as: :cart_orders
   post "/signin" => "sessions#create", as: :sessions
   get "/signin" => "sessions#new", as: :new_sessions
   delete "/signout" => "sessions#destroy", as: :destroy_session
