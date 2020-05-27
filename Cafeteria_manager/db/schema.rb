1f1449dd00b0508bec33807e885b2f5865b7c7e1
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-05-27 10:39:48 +0530

    Add total_price column to orders

--- Cafeteria_manager/db/schema.rb
+++ Cafeteria_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_155922) do
+ActiveRecord::Schema.define(version: 2020_05_27_050853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_155922) do
     t.boolean "add_to_cart"
     t.boolean "place_order"
     t.boolean "order_delivered"
+    t.integer "total_price"
   end
 
   create_table "users", force: :cascade do |t|
